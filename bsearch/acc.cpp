a98f0cae641a7049ae12dd58b6094c53288f24f2
lastshadowpl <xeloxpl@windowslive.com>
2019-09-08 13:20:46 +0200

    I did it!(kinda)

--- bsearch/acc.cpp
+++ bsearch/acc.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <chrono>
+// #include <chrono>
 #include <map>
-#include <vector>
 #define LOGE(x) std::cout << x << std::endl
 
 void aggrInput(int n, int *acc)
@@ -12,8 +11,9 @@ void aggrInput(int n, int *acc)
 
 std::map<int, int> bsDict;
 
-int bsrec(int *arr, int len, int x, int s, int e)
+int bsrec(int *arr, int x, int s, int e)
 {
+  // -------- Check the chache -------- //
   auto entry = bsDict.find(x);
   if (entry != bsDict.end())
   {
@@ -21,92 +21,62 @@ int bsrec(int *arr, int len, int x, int s, int e)
   }
   int i = (s + e + 1) / 2;
 
-  if (arr[i] == x) {
+  // -------- Check the edges -------- //
+  if (arr[s] == x)
+    return s;
+  else if (arr[e] == x)
+    return e;
+  else if (s >= e || x > arr[e] || x < arr[s])
+  {
+    bsDict[x] = -1;
+    return -1;
+  }
+
+  // -------- The main search -------- //
+  if (arr[i] == x)
+  {
     bsDict[x] = i;
     return i;
   }
-  else if (s >= e) {
-    bsDict[x] = 0;
-    return 0;
-  }
   else if (x >= arr[i])
-    return bsrec(arr, len, x, i, e);
+    return bsrec(arr, x, i, e);
   else if (x < arr[i])
-    return bsrec(arr, len, x, s, i - 1);
-}
-
-int bsearch(int *arr, int len, int x, int start)
-{
-  auto entry = bsDict.find(x);
-  if (entry != bsDict.end())
-  {
-    return entry->second;
-  }
-  int s = start;
-  int e = len - 1;
-  int i;
-
-  do
-  {
-    i = (s + e + 1) / 2;
-
-    if (x >= arr[i])
-    {
-      s = i;
-    }
-    else
-    {
-      e = i - 1;
-    }
-  } while (arr[i] != x && s < e);
-
-  bsDict[x] = i;
-
-  return i;
+    return bsrec(arr, x, s, i - 1);
 }
 
 int main()
 {
-  auto start = std::chrono::high_resolution_clock::now();
   int n, q;
 
+  // -------- Get entries -------- //
   std::cin >> n;
   int entries[n];
   aggrInput(n, entries);
 
+  // -------- Get questions -------- //
   std::cin >> q;
   int questions[q];
   aggrInput(q, questions);
 
-  auto end = std::chrono::high_resolution_clock::now();
-  LOGE((end - start).count());
-  LOGE("n: " << n);
-  LOGE("q: " << q);
-
   for (int i = 0; i < q; i++)
   {
-    auto start = std::chrono::high_resolution_clock::now();
-
-    int anIndex = bsrec(entries, n, questions[i], 0, n - 1);
+    int anIndex = bsrec(entries, questions[i], 0, n - 1);
     int qst = questions[i];
     int k = anIndex;
     int j = anIndex + 1;
     int n = 0;
 
-    while (entries[k] == qst && k >= 0)
+    // -------- Question out off range -------- //
+    if (k == -1)
+      n = 0;
+    else
     {
-      n++;
-      k--;
-    }
+      while (entries[k--] == qst)
+        n++;
 
-    while (entries[j] == qst && j <= n)
-    {
-      n++;
-      j++;
+      while (entries[j++] == qst)
+        n++;
     }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    LOGE((end - start).count());
 
     LOGE(n);
   }
